f6399da1e614c2cf7958c3ed181ddacc60f6ded7
garv-aga <garvagarwal2003@gmail.com>
2024-12-30 18:41:57 +0530

    minor fix 2

--- problems/sha256_hash/gnark-sha256/run.sh
+++ problems/sha256_hash/gnark-sha256/run.sh
@@ -5,4 +5,4 @@ go mod -C problems/sha256_hash/SPJ tidy
 go build -C problems/sha256_hash/gnark-sha256
 go build -C problems/sha256_hash/SPJ
 
-problems/sha256_hash/SPJ/SPJ -cpu 16 -largestN 4096 -memory 32768 -prover "problems/sha256_hash/gnark-sha256/gnark-sha256 -mode prove" -time 1200 -verifier "problems/sha256_hash/gnark-sha256/gnark-sha256 -mode verify" -json "spj_output/keccak256_hash/result.json"
+problems/sha256_hash/SPJ/SPJ -cpu 16 -largestN 4096 -memory 32768 -prover "problems/sha256_hash/gnark-sha256/gnark-sha256 -mode prove" -time 1200 -verifier "problems/sha256_hash/gnark-sha256/gnark-sha256 -mode verify" -json "spj_output/sha256_hash/result.json"
